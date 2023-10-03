a3dd2a8f6c18dd9b4de2e3e19830d08ddf0f47ea
Rewan Noufal <rewan.noufal.1998@gmail.com>
2023-10-03 18:16:53 +0300

    first commit

---
+++ src/components/TreeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ node.name }}</h1>
+    </div>
+</template>
+
+<script >
+
+
+export default {
+    props: {
+        node: Object,
+    }
+}
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <treeView
+      :node="root"
+      />
+        
+
+    </div>
   </main>
 </template>
+
+
+<script >
+import TreeView from "../components/TreeView.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      root: {
+        name: 'my computer',
+        "children": [
+          {
+            name: 'documents',
+            children: [
+              {
+                name: "book.pdf"
+              },
+              {
+                name:"Home",
+                children:[
+                  {
+                    name:"music"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    TreeView
+  }
+}
+</script>
